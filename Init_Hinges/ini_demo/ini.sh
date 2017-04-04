aeaf3a641fd7692654acfac63a75f86c43225d39
mrhengyuan <zshi.hy@gmail.com>
2017-04-03 18:07:49 -0700

    Update ini.sh

--- Init_Hinges/ini_demo/ini.sh
+++ Init_Hinges/ini_demo/ini.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# creat the oxDNA topology file and configuration file from the caDNAno design
 python ../cadnano_interface.py 53b.json he
+# substitute the stretched bonds with harmonic restraints on distance
 python ../init_generator.py
 
+# run initial relaxation in a sub folder
 mkdir relax
 cd relax
 oxDNA ../relax.in
 cp last_conf.dat ../ini.conf
 cd ..
 
+# run the initializations step by step
 for dir in ./ini*/
 do
     cd $dir
@@ -18,6 +22,7 @@ do
     cd ..
 done
 
+# build folder for the productive run
 mkdir pro
 cd pro
 cp ../ini.conf ini.conf
